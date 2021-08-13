c7fd12e1cf44758cd233d76e1569a6ea20e38d17
adithyas2000 <adithyasaranga@gmail.com>
2021-08-14 00:51:23 +0530

    added create store ui

---
+++ app/src/main/java/com/akatsuki/greenarcade/CreateStore.java
@@ -0,0 +1,14 @@
+package com.akatsuki.greenarcade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateStore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_store);
+    }
+}
