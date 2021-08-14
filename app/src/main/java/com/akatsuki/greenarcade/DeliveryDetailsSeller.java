e45197fb15e1dff6226c7654a09c5d6754256730
Dinul Liayanage <dinulliyanage@gmail.com>
2021-08-15 02:42:05 +0530

    initial commit

---
+++ app/src/main/java/com/akatsuki/greenarcade/DeliveryDetailsSeller.java
@@ -0,0 +1,14 @@
+package com.akatsuki.greenarcade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliveryDetailsSeller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_details_seller);
+    }
+}
